55c55737f35b885cdd4505960e8b37e95b3ab6ef
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-12-06 09:23:17 -0800

    conctructor created

--- src/main/java/Quote.java
+++ src/main/java/Quote.java
@@ -1,2 +1,18 @@
 public class Quote {
+
+    protected String[] tags;
+    protected String author;
+    protected String likes;
+    protected String text;
+
+    public Quote (String[] tags, String author, String likes, String text) {
+        this.tags = tags;
+        this.author = author;
+        this.likes = likes;
+        this.text = text;
+    }
+
+    public String toString() {
+        return this.text + " --" + this.author;
+    }
 }
